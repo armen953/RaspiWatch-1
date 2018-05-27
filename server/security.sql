86f0633f1f326beecbe68736aafac8fb2fcc64d3
armen953 <armen.mahmedov@gmail.com>
2018-05-27 20:23:51 +0200

    change sql for create defaut admin

--- server/security.sql
+++ server/security.sql
@@ -17,3 +17,12 @@ VALUES (2,1,' /var/www/motionImage/minion.jpg',0,1,'2018-05-26 18:45:20'),
 (3,1,'/var/www/motionImage/jcvd.jpg',0,1,'2018-05-26 18:45:20'),
 (4,1,'/var/www/motionImage/rio.jpg',0,1,'2018-05-26 18:49:15'),
 (5,1,'/var/www/motionImage/minion.jpg',0,8,'2018-05-26 18:49:15');
+
+
+-- CREATE A ADMIN WITH : 
+
+-- Login: root 
+-- MDP: root
+delete from Users where pseudo='root';
+INSERT INTO Users (pseudo, email, password, admin, createdAt, updatedAt)
+VALUES ('root','root@root.fr','$2a$08$TPeudJBApgZJiSYe8VmPd.In8DmSiOKmtnwvFvGEkTE7iRoSH/NVW',1,'2018-05-27 17:13:45','2018-05-27 17:13:45');
